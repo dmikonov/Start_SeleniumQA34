e5c9cae683704553924e63af932ad144c8dc52c7
dmikonov <dmikonov@yahoo.com>
2022-06-15 19:47:46 +0300

    add css class

---
+++ Start_Selenium_QA34/src/test/java/Start.java
@@ -0,0 +1,41 @@
+import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+/*
+public class Start {
+
+    WebDriver wd;
+
+    @Test
+    public void start(){
+        wd = new ChromeDriver();
+       // wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
+        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+        wd.navigate().back();
+        wd.navigate().forward();
+        wd.navigate().refresh();
+        wd.close();
+       // wd.quit();
+    }
+
+    @Test
+    public void firstTest() {
+        wd = new ChromeDriver();
+        wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
+        WebElement loginTab;
+        loginTab.click();
+        WebElement emailTextBox;
+        emailTextBox.click();
+        emailTextBox.clear();
+        emailTextBox.sendKeys("d020797@gmail.com");
+        WebElement passwordTextBox;
+        passwordTextBox.click();
+        passwordTextBox.clear();
+        emailTextBox.sendKeys("Tt12345$");
+        WebElement loginButton;
+        loginButton.click();
+    }
+}
+*/
