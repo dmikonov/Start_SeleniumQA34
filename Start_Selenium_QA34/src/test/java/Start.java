23a6fcfa3141f1d46b1fdccd8c24544c00360575
dmikonov <dmikonov@yahoo.com>
2022-06-15 20:56:32 +0300

    Homework

--- Start_Selenium_QA34/src/test/java/Start.java
+++ Start_Selenium_QA34/src/test/java/Start.java
@@ -1,9 +1,10 @@
 import com.google.common.annotations.VisibleForTesting;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-/*
+
 public class Start {
 
     WebDriver wd;
@@ -24,18 +25,41 @@ public class Start {
     public void firstTest() {
         wd = new ChromeDriver();
         wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
-        WebElement loginTab;
+        WebElement loginTab = wd.findElement(By.cssSelector("[href='/login']"));
         loginTab.click();
-        WebElement emailTextBox;
+
+        WebElement emailTextBox = wd.findElement(By.cssSelector("[placeholder='Email']"));
         emailTextBox.click();
         emailTextBox.clear();
         emailTextBox.sendKeys("d020797@gmail.com");
-        WebElement passwordTextBox;
+
+        WebElement passwordTextBox = wd.findElement(By.cssSelector("[placeholder='Password']"));
         passwordTextBox.click();
         passwordTextBox.clear();
-        emailTextBox.sendKeys("Tt12345$");
-        WebElement loginButton;
+        passwordTextBox.sendKeys("Ww12345$");
+
+        WebElement loginButton = wd.findElement(By.cssSelector("button"));
         loginButton.click();
     }
+
+    @Test
+    public void HomeworkRegistration(){
+        wd = new ChromeDriver();
+        wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
+        WebElement loginTab = wd.findElement(By.cssSelector("[href='/login']"));
+        loginTab.click();
+
+        WebElement emailTextBox = wd.findElement(By.cssSelector("[placeholder='Email']"));
+        emailTextBox.click();
+        emailTextBox.clear();
+        emailTextBox.sendKeys("d020797@yahoo.com");
+
+        WebElement passwordTextBox = wd.findElement(By.cssSelector("[placeholder='Password']"));
+        passwordTextBox.click();
+        passwordTextBox.clear();
+        passwordTextBox.sendKeys("Ww12345$");
+
+        WebElement registrationButton = wd.findElement(By.cssSelector("div.login_login__3EHKB :last-child"));
+        registrationButton.click();
+    }
 }
-*/
--- Start_Selenium_QA34/src/test/java/StartCSS.java
+++ Start_Selenium_QA34/src/test/java/StartCSS.java
@@ -10,18 +10,16 @@ public class StartCSS {
     WebDriver wd;
 
     @Test
-    public void startCssLocators(){
+    public void startCssLocators() {
         wd = new ChromeDriver();
         wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
         WebElement el1 = wd.findElement(By.id("root"));
         WebElement el2 = wd.findElement(By.tagName("a"));
         List<WebElement> list = wd.findElements(By.tagName("a")); //---> 3 "a"
         wd.findElement(By.className("container"));
-      //  wd.findElement(By.name("name"));
+        //  wd.findElement(By.name("name"));
         wd.findElement(By.linkText("LOGIN"));
         wd.findElement(By.partialLinkText("LOG"));
-
-
 
         //////////////////////////////////////
 
